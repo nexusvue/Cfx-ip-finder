8d4a76a43811051d535311094a44d6fa7c435372
rutkuli <44347946+rutkuli@users.noreply.github.com>
2021-02-24 00:47:41 +0200

    update for old one

--- CFXFinder.js
+++ CFXFinder.js
@@ -1,59 +1,66 @@
-
 const Discord = require('discord.js')
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; /*Parempi vaan constiin päivittää*/
 const client = new Discord.Client();
 const prefix = '-'
 
-client.login('token');
+client.login('');
 
-client.on("message", function(message) {
-
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-        const args = message.content.slice(prefix.length).trim().split(' ');
-        const command = args.shift().toLowerCase();
-
-    if (command === 'find') {
-
-        const channel01 = client.channels.cache.find(channel => channel.id === 'channelid');
-
-        if (!args.length) {
-            return channel01.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-
-        do {
-            var req = new XMLHttpRequest();
-            req.open('GET', args, false); // open connection to selected cfx url
-            req.send(null); 
-            var headers = req.getResponseHeader('x-citizenfx-url').toLowerCase();  
-            var name = req.getResponseHeader('x-citizenfx-join-token').toLowerCase();  
-        }while(headers.startsWith('h'))  // sometimes result is url idk why but this makes sure that u don't get https address as result
-
-        if(!headers.startsWith('h')){ // check if the ResponseHeader isn't url
-            const exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-             .setTitle('Ultimate Rat CFX Finderino.')
-     
-       
-             .addFields(
-                { name: 'CFX', value: args },
-                { name: 'Join token', value: name },
-                { name: 'IP Address', value: headers },
-              )
-             .setTimestamp()
-             .setFooter('Ultimate Rat CFX Finderino.');
-            channel01.send(exampleEmbed);
-        }
-    
-
-    }
-
-
-
-
-
+client.on('ready', () => {
+	console.log(`bot, is ready for use.`, "ready");	
 });
 
+	function Main(url, channel) {
+			try {
+				do {
+					var req = new XMLHttpRequest();
+					req.open('GET', url, false);
+					req.send(null);
+					var headers = req.getResponseHeader('x-citizenfx-url');
+				}while(headers.includes('https'))
+				if(headers.includes('https://')) {				
+				} else {
+					const location = client.channels.cache.get(channel);
+					const exampleEmbed = new Discord.MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle('Ultimate Rat CFX Finderino.')
+					.addFields(
+					{ name: 'CFX', value: url },
+					{ name: 'IP Address', value: headers },
+					)
+					.setTimestamp()
+					.setFooter('Ultimate Rat CFX Finderino.');
+					location.send(exampleEmbed);
+				}		
+			} catch(err) {
+			}
+	}
+	
 
+client.on("message", function(message) {
+		if (!message.content.startsWith(prefix) || message.author.bot) return;
+        const args = message.content.slice(prefix.length).trim().split(' ');
+        const command = args.shift().toLowerCase();
+    if (command === 'find') {
+		const link = args[0];
+			if (!args.length) {
+				const exampleEmbed33 = new Discord.MessageEmbed()
+					.setColor('#f00028')
+					.setTitle('Error.')
+					.setDescription(`You didn't provide any arguments, ${message.author}!`)
+					.setTimestamp()
+					.setFooter('Error.');
+						message.channel.send(exampleEmbed33); /*Returnia ei tarvitse käyttää ollenkaan koska kyseessä ei ole c++ ja sen paskat funktiot*/
+		} else if(link.includes('https://')) {
+			const channel = message.channel.id;
+			Main(link, channel);
+		} else {
+			const exampleEmbed33 = new Discord.MessageEmbed()
+			.setColor('#f00028')
+			.setTitle('Error.')
+			.setDescription(`Invalid URL, ${message.author}!`)
+			.setTimestamp()
+			.setFooter('Error.');
+			message.channel.send(exampleEmbed33);
+		}
+	}
+});
